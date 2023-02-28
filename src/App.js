ac840847ef55ccd5d58130a71ec9587208c648fd
alefCS50 <allan.efendic@icloud.com>
2023-02-28 14:12:07 +0100

    drum-machine(final)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const buttonToSound = {
+    Q: "Heater1",
+    W: "Heater2",
+    E: "Heater3",
+    A: "Heater4",
+    S: "Clap",
+    D: "Open-HH",
+    Z: "Kick-n'-Hat",
+    X: "Kick",
+    C: "Closed-HH"
+
+  }
+  const [sound, setSound] = useState("");
+
+  const keys = "QWEASDZXCqweasdzxc";
+  function PlayAudio(id) {
+    let aud = document.getElementById(id);
+    aud.play();
+    setSound(buttonToSound[id]);
+    console.log(sound);
+  }
+
+  function PlayAudioOnKey(event) {
+    if(keys.includes(event.key)) {
+      let aud = document.getElementById(event.key);
+      aud.play();
+    }
+  }
+
+  document.addEventListener('keydown', PlayAudioOnKey)
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="drum-machine">
+      <div id="display">
+        <div id="buttons">
+          <div className='drum-pad' id="Heater1" onClick={() => PlayAudio("Q")}>Q <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" id="Q" className='clip'/></div>
+          <div className='drum-pad' id="Heater2" onClick={() => PlayAudio("W")}>W <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" id="W" className='clip'/></div>
+          <div className='drum-pad' id="Heater3" onClick={() => PlayAudio("E")}>E <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" id="E" className='clip'/></div>
+          <div className='drum-pad' id="Heater4" onClick={() => PlayAudio("A")}>A <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" id="A" className='clip'/></div>
+          <div className='drum-pad' id="Clap" onClick={() => PlayAudio("S")}>S <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" id="S" className='clip'/></div>
+          <div className='drum-pad' id="Open-HH" onClick={() => PlayAudio("D")}>D <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" id="D" className='clip'/></div>
+          <div className='drum-pad' id="Kick-n'-Hat" onClick={() => PlayAudio("Z")}>Z <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" id="Z" className='clip'/></div>
+          <div className='drum-pad' id="Kick" onClick={() => PlayAudio("X")}>X <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" id="X" className='clip'/></div>
+          <div className='drum-pad' id="Closed-HH" onClick={() => PlayAudio("C")}>C <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" id="C" className='clip'/></div>
+        </div>
+
+        <br></br>
+        <div id="sound-wrapper">
+          <div id="sound-box">{sound}</div>
+        </div>
+      </div>
+      
     </div>
   );
 }
